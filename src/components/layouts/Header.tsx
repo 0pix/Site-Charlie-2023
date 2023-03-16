113af0002a191141fb9aa4db626f9df4f1e1e1f7
Opix <opixgamer@gmail.com>
2023-03-16 03:21:29 +0100

    Header img now dynamic

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 import "./Header.scss"
+import json from "../../configSite.json"
 
 type Props = {
+
   name: string;
 };
 
+
+
+
 const Header: FC<Props> = ({ name }) => {
-  return <header>Hello {name}!</header>;
+const testjson :string = json.imageHeader
+console.log(testjson)
+  return <header> <img src={`../img/${testjson}`}  alt=""/> Hello {name}!</header>;
 };
 
 export default Header;
